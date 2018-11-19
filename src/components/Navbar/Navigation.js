0a994db40d2ab757903eaf058034bf9bb041e666
Carolina <carolinatirado92@gmail.com>
2018-11-19 10:03:19 +0100

    Navbar et index.css + app.css

---
+++ src/components/Navbar/Navigation.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './index.css';
+import { NavLink } from 'react-router-dom'
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        }
+    }
+
+    handleClick() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+            <header>
+                <div className={this.state.isOpen ? 'menu_bar_open' : 'menu_bar'} onClick={() => this.handleClick()}>
+
+                    <span className="icon-menu"></span>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/" activeClassName="selected" className="icon-home">HOME</NavLink></li>
+                            <li><NavLink to="/CocktailList" className="icon-glass2">COCKTAILS</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Navigation;
