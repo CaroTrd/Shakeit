132f73f07e155a77592b28d941be256e68eeaa94
celestinebonaert <celestine.bonaert@gmail.com>
2018-11-21 15:03:46 +0100

    Mise en page ingredientlist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import Home from './components/Home/index';
-import CocktailList from './components/CocktailList/index';
+//import Home from './components/Home/index';
+//import CocktailList from './components/CocktailList/index';
 import BarreDeRecherche from './components/BarreDeRecherche/index';
-import Navigation from './components/Navbar/Navigation';
-import DisplayRecette from './components/Recette/DisplayRecette';
+import Ingredients from './components/PageIngredient/index';
+//import Navigation from './components/Navbar/Navigation';
+//import DisplayRecette from './components/Recette/DisplayRecette';
 
 
 
@@ -14,12 +15,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation /> 
+        {/*<Navigation />*/ }
         <BarreDeRecherche/>
           <Switch>
-              <Route exact path="/" component={Home} />
+          <Route exact path="/ingredients" component={Ingredients} />
+              {/*<Route exact path="/" component={Home} />
               <Route path="/cocktaillist" component={CocktailList}/>
-              <Route path="/recette" component={DisplayRecette} />
+              <Route path="/recette" component={DisplayRecette} /> */}
           </Switch>
 
       </div>
