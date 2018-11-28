f23c142db0692e8c673f0a02c7dd290195b12fb5
Audrey Mertens <audrey.mertens@hotmail.be>
2018-11-28 17:02:26 +0100

    vrai finition css page cocktail

--- src/components/cocktailList/index.js
+++ src/components/cocktailList/index.js
@@ -6,62 +6,62 @@ import './index.css';
 
 
 class CocktailList extends Component {
-    
-    constructor (props){
+
+    constructor(props) {
         super(props);
         this.state = {
-            cocktailName : [],
+            cocktailName: [],
             visible: 6,
         }
-        
+
     }
 
     loadMore() {
         this.setState((prev) => {
-          return {visible: prev.visible + 6};
+            return { visible: prev.visible + 6 };
         });
-      }
+    }
 
     getCocktailName(cocktail) {
 
         this.setState(
-            {cocktailName : cocktail}
+            { cocktailName: cocktail }
         )
     }
 
-    
-  
+
+
     render() {
         const cocktail = this.state.cocktailName.slice(0, this.state.visible).map((elem, index) => (
-            <li className = "cocktail"
-            key={index}>
+            <li className="cocktail"
+                key={index}>
                 <div className="container-cocktail">
                     <img src={elem.strDrinkThumb} alt="" />
-                    <p className = "cocktailDescription">{elem.strDrink}</p>
+                    <p className="cocktailDescription">{elem.strDrink}</p>
                 </div>
             </li>
         ))
 
-        
-    return (
-        <Fragment>
-            <div className="page">
-                <h1 className = "page-title">SEARCH FOR YOUR COCKTAIL HERE</h1> 
-                <BarreDeRecherche callback ={(cocktail) => this.getCocktailName(cocktail)} type="cocktail"/>
-                    <ul className="cocktail-list"> 
+
+        return (
+            <Fragment>
+                <div className="page">
+                    <h1 className="page-title">SEARCH FOR YOUR COCKTAIL HERE</h1>
+                    <BarreDeRecherche callback={(cocktail) => this.getCocktailName(cocktail)} type="cocktail" />
+                    <ul className="cocktail-list">
                         {cocktail}
                     </ul>
                     {this.state.visible < this.state.cocktailName.length &&
-             <button onClick={() => this.laodMore()} type="button" className="cta">Load more</button>
-            }
-            </div>
-           
+                        <button onClick={() => this.laodMore()} type="button" className="cta">Load more</button>
+                    }
+                </div>
 
-        </Fragment>
-        
-    );
-   
-  }
+
+            </Fragment>
+
+        );
+
+    }
 }
 
 export default CocktailList;
