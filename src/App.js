49686f932594aa40b0a154170689c4ff4def9a38
Carolina <carolinatirado92@gmail.com>
2018-11-28 16:06:26 +0100

    Configuration de Spotify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import Recette from './components/Recette/index';
 
 import './App.css';
-import Home from './components/Home/index';
-import CocktailList from './components/CocktailList/index';
-import BarreDeRecherche from './components/BarreDeRecherche/index';
-import Navigation from './components/Navbar/Navigation';
-import DisplayRecette from './components/Recette/DisplayRecette';
 
 
 
@@ -14,14 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation /> 
-        <BarreDeRecherche/>
-          <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/cocktaillist" component={CocktailList}/>
-              <Route path="/recette" component={DisplayRecette} />
-          </Switch>
-
+        <Recette />
       </div>
     );
   }
