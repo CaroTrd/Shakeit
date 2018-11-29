65bbaf343742218d47ad39ead98d732a1664bd83
Carolina <carolinatirado92@gmail.com>
2018-11-29 16:54:58 +0100

    Implémentation du API Spotify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import Recette from './components/Recette/index';
+import { withCookies, Cookies } from 'react-cookie';
 
 import './App.css';
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.cookies = new Cookies();
+  
+  }
+
+  componentDidMount() {
+   /* const config = {
+      method :"post",
+      header:{
+        'Content-Type': "application/x-www-form-urlencoded"
+      },
+      data:"refresh_token=NgCXRK...MzYjw",
+
+    }
+    const trouducul= "https://accounts.spotify.com/api/refresh" ;
+    fetch(trouducul,config)
+    .then(response => response.json())
+    .then(data => {
+      console.log('data', data)
+     this.cookies.set('access', data)
+    })*/
+  }
+
   render() {
+  
     return (
+      
       <div className="App">
         <Recette />
       </div>
@@ -15,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withCookies(App);
--- src/components/Recette/ConnectSpotify.js
+++ src/components/Recette/ConnectSpotify.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 
 class ConnectSpotify extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cocktailName: 'Mertens',
+            images: "",
+            url: "",
+
+        }
+    }
+
+    componentDidMount() {
+        const source = 'https://api.spotify.com/v1/search?' + 'q=' + this.state.cocktailName + '&type=playlist&limit=1';
+        const config = {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": 'Bearer ' + 'BQDUkk9FI4oX05IoFkAmqKLKs6fO_unscvV99Ve39TPUWj_gEQhyswGt7gaywNQM0tOZrQv5BfdSnpaDAq4MMWgCwK8t0yN9VjD7-9Ls_cv6GkUwqxS3lyw-vXAUiw7Hnp5Ez9tRG5PBfO9yx-5pB1Lm8emSD7MugS8rSVN8041GWq-LAKHLLkGhwNvY4Kh61OKG2zO9V3jZ29wszZUx2klKuY8'
+            }
+        }
+        fetch(source, config)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (!data.playlists.items.length > 0) {
+                    this.setState({
+                        url: '37i9dQZF1DWWzhxZzVxMP3'
+                    })
+                } else {
+                    this.setState({
+                        url: data.playlists.items[0].id
+                    })
+                }
+            })
+    }
 
     render() {
+        console.log(this.state.url)
         return (
             <div className="play">
-                <a href="http://localhost:8888">
-                <button className="Spotify">
-                    <p className="description">Listen to music here</p>
-                    <img src="https://static.thenounproject.com/png/115005-200.png" alt="spotify" className="spotify-logo" />
-                </button>
-                </a>
+                <iframe src={`https://open.spotify.com/embed/playlist/${this.state.url}`} width="300" height="380" frameBorder="0" allowtransparency="true">
+                </iframe>
             </div >
         );
     }
