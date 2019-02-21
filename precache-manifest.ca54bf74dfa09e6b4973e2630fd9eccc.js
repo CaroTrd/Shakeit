1922abcf08589e0ab1ca60db803a82c5e72a9951
Audrey Mertens <audrey.mertens@hotmail.be>
2019-02-21 16:01:04 +0100

    Updates

---
+++ precache-manifest.ca54bf74dfa09e6b4973e2630fd9eccc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a61a9d488c58a39b18ef5972ccc34408",
+    "url": "/static/media/logo.a61a9d48.svg"
+  },
+  {
+    "revision": "6bc20041bf3420a272b8",
+    "url": "/static/css/main.055d68c7.chunk.css"
+  },
+  {
+    "revision": "16107de4b9584c33598f",
+    "url": "/static/js/1.16107de4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "607da02822dd424c6b032bfba0cf7187",
+    "url": "/static/media/recettefond.607da028.jpg"
+  },
+  {
+    "revision": "6bc20041bf3420a272b8",
+    "url": "/static/js/main.6bc20041.chunk.js"
+  },
+  {
+    "revision": "ddd8fb290f0e1215ade0fec3dde9c5ff",
+    "url": "/static/media/icomoon.ddd8fb29.ttf"
+  },
+  {
+    "revision": "f56cdf8e91b03d6127cfca393eeab505",
+    "url": "/static/media/icomoon.f56cdf8e.eot"
+  },
+  {
+    "revision": "825448a724c171e4877a2945ef3abaf0",
+    "url": "/static/media/icomoon.825448a7.svg"
+  },
+  {
+    "revision": "785caf1f5823058973ef813829a3fb06",
+    "url": "/static/media/icomoon.785caf1f.woff"
+  },
+  {
+    "revision": "4de1ac409685b3b78f280495f9475d34",
+    "url": "/index.html"
+  }
+];
